8382908341fee927d48d7057a85073b95f0a6e9c
Tomasz Cygal <tomasz.cygal@ibm.com>
2021-02-08 18:20:33 +0100

    added ex3.py

--- ex3.py
+++ ex3.py
@@ -4,3 +4,5 @@ print("Total number of chickens")
 print("Kury", 25 + 30 / 6)
 print("Is it true, that 3 + 2 < 5 - 7")
 print(3 + 2 < 5 - 7)
+print("Koguty", 100 - 25 * 3 % 4) # w dzialaniu uzyte  jest modulo czyli: 25 * 3 = 75 % 4 = 3
+# a nstepnie 100 - 3 (reszta z dzielenia 75 / 4)
